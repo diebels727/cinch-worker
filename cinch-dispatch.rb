011c137fcfb8e6a20f3b72c8b37de6957f95862f
diebels727 <diebelsalternative@hotmail.com>
2014-08-10 19:18:07 -0400

    rudimentary dispatch

--- cinch-dispatch.rb
+++ cinch-dispatch.rb
@@ -30,14 +30,30 @@ get '/:server/:channel' do
   server = params[:server]
   channel = params[:channel]
   server.gsub!("-","\.")
+
   if !File.exists?("#{server}/##{channel}/lock")
-    pid = Process.spawn("cinch-worker.rb --server #{server} --channel #{channel} --output --silent --log show")
     FileUtils.mkpath("#{server}/##{channel}")
+    show = File.open("#{server}/##{channel}/show.log",'w')
+    show.puts "Initializing ..."
+
+    pid = Process.spawn("cinch-worker.rb --server #{server} --channel #{channel} --output --silent --log show")
     lockfile = File.open("#{server}/##{channel}/lock",'w')
     lockfile.puts(pid)
     Process.detach(pid)
+
+    show.puts "Waiting for chatter ..."
+    show.close
+
     lockfile.close
   end
+  show = File.read("#{server}/##{channel}/show.log")
+  show.gsub!(/\n/, '<br>')
+  "<html>
+    <body>
+      <h1>#{server}/##{channel}</h1>
+      #{show}
+    </body>
+  </html>"
 end
 
 
