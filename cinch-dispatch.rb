0a0d06460c9b6d0d17f013c92f9dc7448e0aaa2e
diebels727 <diebelsalternative@hotmail.com>
2014-08-10 18:31:59 -0400

    Add locking.

--- cinch-dispatch.rb
+++ cinch-dispatch.rb
@@ -1,15 +1,26 @@
+#!/usr/bin/ruby
+
 require 'sinatra'
 require 'pry'
 
 #do nothing
 get '/' do;end
 
+get '/:server/:channel/unlock' do
+  server = params[:server]
+  channel = params[:channel]
+  server.gsub!("-","\.")
+  File.delete("#{server}/#{channel}/lock")
+end
+
 get '/:server/:channel' do
   server = params[:server]
   channel = params[:channel]
   server.gsub!("-","\.")
-  binding.pry
   pid = Process.spawn("cinch-worker.rb --server #{server} --channel #{channel} --output --silent")
+  lockfile = File.open("#{server}/#{channel}/lock",'w')
+  lockfile.puts(pid)
   Process.detach(pid)
-
+  lockfile.close
 end
+
